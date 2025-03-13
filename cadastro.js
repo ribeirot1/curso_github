68f466aa8b66042706871b229fe65612f5d34939
devribeirot <ribeirot123@gmail.com>
2025-03-13 15:35:46 -0300

    adicionando funcionalidade de msg

---
+++ cadastro.js
@@ -0,0 +1,14 @@
+function signUp(email){
+    return email;
+}
+
+
+//pegando email e cadastrando na plataforma
+
+
+
+function mostraMensagem(){
+    resturn "Bem vindo ao sistema";
+}
+
+//adicionando função de msg
